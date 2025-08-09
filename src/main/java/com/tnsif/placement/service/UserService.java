c05a718d88d00ce08a6d223dd889451cdd0778e6
Adhil <adhilquazi@gmail.com>
2025-08-09 14:58:37 +0530

    Create UserService.java

---
+++ src/main/java/com/tnsif/placement/service/UserService.java
@@ -0,0 +1,31 @@
+package com.tnsif.placement.service;
+
+import com.tnsif.placement.entity.User;
+import com.tnsif.placement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
